969c4971c2156181f36831f749083dcc92e3f939
Tony Won <tonywon89@gmail.com>
2016-04-22 12:01:10 -0700

    Start implementing index component to render

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,4 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-BenchStore = require('./stores/bench_store');
-ApiUtil = require('./util/api_util');
+// BenchStore = require('./stores/bench_store');
+// ApiUtil = require('./util/api_util');
+var Index = require("./components/index");
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(<Index />, document.getElementById("root"));
+});
---
+++ frontend/components/index.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+var BenchStore = require("../stores/bench_store");
+
+var Index = React.createClass({
+  render: function () {
+    return (
+      <div>This works</div>
+    );
+  }
+});
+
+module.exports = Index;
